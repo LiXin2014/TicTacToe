541caca8dc7b7106db67ac81cdb35e6e0fa02b36
Xin Li <xili3@microsoft.com>
2023-03-01 17:10:27 -0800

    format

--- vanillaDOM/index.ts
+++ vanillaDOM/index.ts
@@ -6,79 +6,79 @@ const COL_COUNT = 3;
 type CellContent = HTMLButtonElement | undefined;
 
 type GameBoard = [[CellContent, CellContent, CellContent],
-                  [CellContent, CellContent, CellContent],
-                  [CellContent, CellContent, CellContent]]
+    [CellContent, CellContent, CellContent],
+    [CellContent, CellContent, CellContent]]
 
 type CurrentMove = "X" | "O";
 type Winner = "X" | "O" | "Tie" | undefined;
 
-let gameBoard : GameBoard = [[undefined, undefined, undefined], 
-                             [undefined, undefined, undefined], 
-                             [undefined, undefined, undefined]]
+let gameBoard: GameBoard = [[undefined, undefined, undefined],
+[undefined, undefined, undefined],
+[undefined, undefined, undefined]]
 let current: CurrentMove;
-let movesMade: 0|1|2|3|4|5|6|7|8|9 = 0;
+let movesMade: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 = 0;
 let gameOver = false;
 let winner: Winner;
 
 function onClick(event: MouseEvent) {
-    if(event.target) {
+    if (event.target) {
         const button = event.target as HTMLButtonElement;
-        if(button.innerText !== "" || gameOver) {
+        if (button.innerText !== "" || gameOver) {
             return;
         }
         button.innerText = current;
         movesMade++;
     }
-    
+
     winner = checkWinLose();
-    
-    if(winner) {
+
+    if (winner) {
         gameOver = true;
     }
     updateStatus();
-    current = current === "X" ? "O": "X";
+    current = current === "X" ? "O" : "X";
 }
 
 function checkWinLose(): Winner | undefined {
     //check rows
-    if(gameBoard[0][0]?.innerText === gameBoard[0][1]?.innerText && gameBoard[0][0]?.innerText === gameBoard[0][2]?.innerText) {
+    if (gameBoard[0][0]?.innerText === gameBoard[0][1]?.innerText && gameBoard[0][0]?.innerText === gameBoard[0][2]?.innerText) {
         return gameBoard[0][0]?.innerText as Winner;
     }
-    if(gameBoard[1][0]?.innerText === gameBoard[1][1]?.innerText && gameBoard[1][0]?.innerText === gameBoard[1][2]?.innerText) {
+    if (gameBoard[1][0]?.innerText === gameBoard[1][1]?.innerText && gameBoard[1][0]?.innerText === gameBoard[1][2]?.innerText) {
         return gameBoard[1][0]?.innerText as Winner;
     }
-    if(gameBoard[2][0]?.innerText === gameBoard[2][1]?.innerText && gameBoard[2][0]?.innerText === gameBoard[2][2]?.innerText) {
+    if (gameBoard[2][0]?.innerText === gameBoard[2][1]?.innerText && gameBoard[2][0]?.innerText === gameBoard[2][2]?.innerText) {
         return gameBoard[2][0]?.innerText as Winner;
     }
     //check columns
-    if(gameBoard[0][0]?.innerText === gameBoard[1][0]?.innerText && gameBoard[0][0]?.innerText === gameBoard[2][0]?.innerText) {
+    if (gameBoard[0][0]?.innerText === gameBoard[1][0]?.innerText && gameBoard[0][0]?.innerText === gameBoard[2][0]?.innerText) {
         return gameBoard[0][0]?.innerText as Winner;
     }
-    if(gameBoard[0][1]?.innerText === gameBoard[1][1]?.innerText && gameBoard[0][1]?.innerText === gameBoard[2][1]?.innerText) {
+    if (gameBoard[0][1]?.innerText === gameBoard[1][1]?.innerText && gameBoard[0][1]?.innerText === gameBoard[2][1]?.innerText) {
         return gameBoard[0][1]?.innerText as Winner;
     }
-    if(gameBoard[0][2]?.innerText === gameBoard[1][2]?.innerText && gameBoard[0][2]?.innerText === gameBoard[2][2]?.innerText) {
+    if (gameBoard[0][2]?.innerText === gameBoard[1][2]?.innerText && gameBoard[0][2]?.innerText === gameBoard[2][2]?.innerText) {
         return gameBoard[0][2]?.innerText as Winner;
     }
     //check diagnols
-    if(gameBoard[0][0]?.innerText === gameBoard[1][1]?.innerText && gameBoard[0][0]?.innerText === gameBoard[2][2]?.innerText) {
+    if (gameBoard[0][0]?.innerText === gameBoard[1][1]?.innerText && gameBoard[0][0]?.innerText === gameBoard[2][2]?.innerText) {
         return gameBoard[0][0]?.innerText as Winner;
     }
-    if(gameBoard[0][2]?.innerText === gameBoard[1][1]?.innerText && gameBoard[0][2]?.innerText === gameBoard[2][0]?.innerText) {
+    if (gameBoard[0][2]?.innerText === gameBoard[1][1]?.innerText && gameBoard[0][2]?.innerText === gameBoard[2][0]?.innerText) {
         return gameBoard[0][2]?.innerText as Winner;
     }
 
-    if(movesMade === 9) {
+    if (movesMade === 9) {
         return "Tie";
     }
 }
 
 function createButtons() {
-    for(let row = 0; row<ROW_COUNT; row++) {
+    for (let row = 0; row < ROW_COUNT; row++) {
         const rows = document.createElement("div");
         rows.classList.add("row");
 
-        for(let col=0; col<COL_COUNT; col++) {
+        for (let col = 0; col < COL_COUNT; col++) {
             const button = document.createElement("button");
             button.setAttribute("data-rows", row.toString());
             button.setAttribute("data-columns", col.toString());
@@ -93,14 +93,14 @@ function createButtons() {
 
 function createStatus() {
     const nextStatusText = document.createElement("p");
-    nextStatusText.id="next";
+    nextStatusText.id = "next";
     nextStatusText.innerText = `Next: ${current === "X" ? "O" : "X"}`;
     gameStatus?.appendChild(nextStatusText);
 }
 
 function updateStatus() {
     const nextStatusText = document.querySelector("#next") as HTMLParagraphElement;
-    if(gameOver) {
+    if (gameOver) {
         nextStatusText.innerText = `Winner: ${winner}`;
     } else {
         nextStatusText.innerText = `Next: ${current === "X" ? "O" : "X"}`;
@@ -109,14 +109,14 @@ function updateStatus() {
 
 function createResetButton() {
     const button = document.createElement("button");
-    button.id="reset";
+    button.id = "reset";
     button.innerText = "Reset";
     gameStatus?.appendChild(button);
     button.addEventListener("click", (event) => {
-        while(board?.firstChild) {
+        while (board?.firstChild) {
             board.removeChild(board.firstChild);
         }
-        while(gameStatus?.firstChild) {
+        while (gameStatus?.firstChild) {
             gameStatus.removeChild(gameStatus.firstChild);
         }
         init();
