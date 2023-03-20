3cee203370d66d726b55798df95e97d7184bb742
Xin Li <xinli601@gmail.com>
2023-03-19 21:51:43 -0700

    add time travel.

--- vanillaDOM/index.js
+++ vanillaDOM/index.js
@@ -1,30 +1,46 @@
 var board = document.querySelector(".board");
-var gameStatus = document.querySelector(".status");
+var statusText = document.querySelector(".status");
 var ROW_COUNT = 3;
 var COL_COUNT = 3;
-var gameBoard = [["", "", ""],
-    ["", "", ""],
-    ["", "", ""]];
-var current;
+var gameStatus = {
+    gameBoard: [["", "", ""],
+        ["", "", ""],
+        ["", "", ""]],
+    current: "X",
+    gameOver: false,
+    winner: "",
+    currentStep: 0
+};
+var steps = [];
 var movesMade = 0;
-var gameOver = false;
-var winner;
+/*let gameBoard: GameBoard = [["", "", ""],
+                            ["", "", ""],
+                            ["", "", ""]]
+
+let current: CurrentMove;
+let movesMade: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 = 0;
+let gameOver = false;
+let winner: Winner;*/
 function onClick(event, row, col) {
+    var gameBoard = gameStatus.gameBoard, current = gameStatus.current, gameOver = gameStatus.gameOver, currentStep = gameStatus.currentStep;
     if (event.target) {
         var button = event.target;
-        if (button.innerText !== "" || gameOver) {
+        if (button.innerText !== "" || gameOver || currentStep < movesMade) {
             return;
         }
         button.innerText = current;
         movesMade++;
+        gameStatus.currentStep++;
     }
     gameBoard[row][col] = current;
-    winner = checkWinLose();
-    if (winner !== "") {
-        gameOver = true;
+    gameStatus.winner = checkWinLose();
+    if (gameStatus.winner !== "") {
+        gameStatus.gameOver = true;
     }
     updateStatus();
-    current = current === "X" ? "O" : "X";
+    gameStatus.current = current === "X" ? "O" : "X";
+    addToTimeTravel();
+    steps.push(deepCopyGameStatus(gameStatus));
 }
 var victories = [
     [
@@ -69,6 +85,7 @@ var victories = [
     ],
 ];
 function checkWinLose() {
+    var gameBoard = gameStatus.gameBoard;
     if (movesMade === 9) {
         return "Tie";
     }
@@ -106,10 +123,11 @@ function createButtons() {
 function createStatus() {
     var nextStatusText = document.createElement("p");
     nextStatusText.id = "next";
-    nextStatusText.innerText = "Next: " + (current === "X" ? "O" : "X");
-    gameStatus === null || gameStatus === void 0 ? void 0 : gameStatus.appendChild(nextStatusText);
+    nextStatusText.innerText = "Next: " + (gameStatus.current === "X" ? "O" : "X");
+    statusText === null || statusText === void 0 ? void 0 : statusText.appendChild(nextStatusText);
 }
 function updateStatus() {
+    var current = gameStatus.current, gameOver = gameStatus.gameOver, winner = gameStatus.winner;
     var nextStatusText = document.querySelector("#next");
     if (gameOver) {
         nextStatusText.innerText = "Winner: " + winner;
@@ -122,29 +140,90 @@ function createResetButton() {
     var button = document.createElement("button");
     button.id = "reset";
     button.innerText = "Reset";
-    gameStatus === null || gameStatus === void 0 ? void 0 : gameStatus.appendChild(button);
+    statusText === null || statusText === void 0 ? void 0 : statusText.appendChild(button);
     button.addEventListener("click", function (event) {
         while (board === null || board === void 0 ? void 0 : board.firstChild) {
             board.removeChild(board.firstChild);
         }
-        while (gameStatus === null || gameStatus === void 0 ? void 0 : gameStatus.firstChild) {
-            gameStatus.removeChild(gameStatus.firstChild);
+        while (statusText === null || statusText === void 0 ? void 0 : statusText.firstChild) {
+            statusText.removeChild(statusText.firstChild);
         }
         init();
     });
+}
+function createTimeTravel() {
+    var lists = document.createElement("ul");
+    lists.id = "timeTravel";
+    var list = document.createElement("li");
+    list.innerText = "Go to step 0";
+    list.addEventListener("click", function (event) { return onStepClicked(event, 0); });
+    lists.appendChild(list);
+    statusText === null || statusText === void 0 ? void 0 : statusText.appendChild(lists);
+}
+function addToTimeTravel() {
+    var lists = document.querySelector("#timeTravel");
+    var childrenLen = lists.children.length || 0;
+    var list = document.createElement("li");
+    list.innerText = "Go to step " + childrenLen;
+    list.addEventListener("click", function (event) { return onStepClicked(event, childrenLen); });
+    lists.appendChild(list);
+}
+function onStepClicked(event, step) {
+    console.log("step: " + step);
+    var gameStatusToShow = deepCopyGameStatus(steps[step]);
+    gameStatus = gameStatusToShow;
+    updateStatus();
+    updateBoard();
+}
+// update UI board based on gameStatus.board
+function updateBoard() {
+    var boardElement = document.querySelector(".board");
+    var rowsElement = boardElement === null || boardElement === void 0 ? void 0 : boardElement.querySelectorAll(".row");
+    for (var row = 0; row < ROW_COUNT; row++) {
+        var rowElement = rowsElement[row];
+        var buttonsElement = rowElement.querySelectorAll("button");
+        for (var col = 0; col < COL_COUNT; col++) {
+            var buttonElement = buttonsElement[col];
+            buttonElement.innerText = gameStatus.gameBoard[row][col];
+        }
+    }
+}
+function deepCopyGameStatus(gameStatusToCopy) {
+    var newGameStatus = {
+        gameBoard: [["", "", ""],
+            ["", "", ""],
+            ["", "", ""]],
+        current: gameStatusToCopy.current,
+        gameOver: gameStatusToCopy.gameOver,
+        winner: gameStatusToCopy.winner,
+        currentStep: gameStatusToCopy.currentStep
+    };
+    for (var row = 0; row < ROW_COUNT; row++) {
+        for (var col = 0; col < COL_COUNT; col++) {
+            newGameStatus.gameBoard[row][col] = gameStatusToCopy.gameBoard[row][col];
+        }
+    }
+    return newGameStatus;
 }
 function createGame() {
     createButtons();
     createStatus();
     createResetButton();
+    createTimeTravel();
 }
 function init() {
     createGame();
-    gameBoard = [["", "", ""],
-        ["", "", ""],
-        ["", "", ""]];
-    current = "X";
+    gameStatus = {
+        gameBoard: [["", "", ""],
+            ["", "", ""],
+            ["", "", ""]],
+        current: "X",
+        gameOver: false,
+        winner: "",
+        currentStep: 0
+    };
     movesMade = 0;
-    gameOver = false;
+    steps = [];
+    steps.push(deepCopyGameStatus(gameStatus));
 }
 init();
--- vanillaDOM/index.ts
+++ vanillaDOM/index.ts
@@ -1,5 +1,5 @@
 const board = document.querySelector(".board");
-const gameStatus = document.querySelector(".status");
+const statusText = document.querySelector(".status");
 const ROW_COUNT = 3;
 const COL_COUNT = 3;
 
@@ -12,90 +12,118 @@ type GameBoard = [[CellContent, CellContent, CellContent],
 type CurrentMove = "X" | "O";
 type Winner = "X" | "O" | "Tie" | "";
 
-let gameBoard: GameBoard = [["", "", ""],
+type GameStatus = {
+    gameBoard: GameBoard,
+    current: CurrentMove,
+    gameOver: boolean,
+    winner: Winner,
+    currentStep: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9   // in which step, to check if we are currently in time travel
+}
+
+let gameStatus: GameStatus = {
+    gameBoard: [["", "", ""],
+    ["", "", ""],
+    ["", "", ""]],
+    current: "X",
+    gameOver: false,
+    winner: "",
+    currentStep: 0
+}
+
+let steps: GameStatus[] = [];
+let movesMade: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 = 0;
+
+/*let gameBoard: GameBoard = [["", "", ""],
                             ["", "", ""],
                             ["", "", ""]]
 
 let current: CurrentMove;
 let movesMade: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 = 0;
 let gameOver = false;
-let winner: Winner;
+let winner: Winner;*/
+
 
 function onClick(event: MouseEvent, row: number, col: number) {
+    let { gameBoard, current, gameOver, currentStep } = gameStatus;
     if (event.target) {
         const button = event.target as HTMLButtonElement;
-        if (button.innerText !== "" || gameOver) {
+        if (button.innerText !== "" || gameOver || currentStep < movesMade) {
             return;
         }
         button.innerText = current;
         movesMade++;
+        gameStatus.currentStep++;
     }
 
     gameBoard[row][col] = current;
-    winner = checkWinLose();
+    gameStatus.winner = checkWinLose();
 
-    if (winner !== "") {
-        gameOver = true;
+    if (gameStatus.winner !== "") {
+        gameStatus.gameOver = true;
     }
     updateStatus();
-    current = current === "X" ? "O" : "X";
+    gameStatus.current = current === "X" ? "O" : "X";
+
+    addToTimeTravel();
+    steps.push(deepCopyGameStatus(gameStatus));
 }
 
 type Coordinate = [number, number];
 type Victory = [Coordinate, Coordinate, Coordinate];
 const victories: Victory[] = [
-  [
-    [0, 0],
-    [0, 1],
-    [0, 2],
-  ],
-  [
-    [1, 0],
-    [1, 1],
-    [1, 2],
-  ],
-  [
-    [2, 0],
-    [2, 1],
-    [2, 2],
-  ],
-  [
-    [0, 0],
-    [1, 0],
-    [2, 0],
-  ],
-  [
-    [0, 1],
-    [1, 1],
-    [2, 1],
-  ],
-  [
-    [0, 2],
-    [1, 2],
-    [2, 2],
-  ],
-  [
-    [0, 0],
-    [1, 1],
-    [2, 2],
-  ],
-  [
-    [0, 2],
-    [1, 1],
-    [2, 0],
-  ],
+    [
+        [0, 0],
+        [0, 1],
+        [0, 2],
+    ],
+    [
+        [1, 0],
+        [1, 1],
+        [1, 2],
+    ],
+    [
+        [2, 0],
+        [2, 1],
+        [2, 2],
+    ],
+    [
+        [0, 0],
+        [1, 0],
+        [2, 0],
+    ],
+    [
+        [0, 1],
+        [1, 1],
+        [2, 1],
+    ],
+    [
+        [0, 2],
+        [1, 2],
+        [2, 2],
+    ],
+    [
+        [0, 0],
+        [1, 1],
+        [2, 2],
+    ],
+    [
+        [0, 2],
+        [1, 1],
+        [2, 0],
+    ],
 ];
 
 function checkWinLose(): Winner {
+    let { gameBoard } = gameStatus;
     if (movesMade === 9) {
         return "Tie";
     }
 
-    for(let victory of victories) {
+    for (let victory of victories) {
         const cell1 = gameBoard[victory[0][0]][victory[0][1]];
         const cell2 = gameBoard[victory[1][0]][victory[1][1]];
         const cell3 = gameBoard[victory[2][0]][victory[2][1]];
-        if(cell1 !== "" && cell1 === cell2 && cell1 === cell3) {
+        if (cell1 !== "" && cell1 === cell2 && cell1 === cell3) {
             return cell1;
         }
     }
@@ -123,11 +151,12 @@ function createButtons() {
 function createStatus() {
     const nextStatusText = document.createElement("p");
     nextStatusText.id = "next";
-    nextStatusText.innerText = `Next: ${current === "X" ? "O" : "X"}`;
-    gameStatus?.appendChild(nextStatusText);
+    nextStatusText.innerText = `Next: ${gameStatus.current === "X" ? "O" : "X"}`;
+    statusText?.appendChild(nextStatusText);
 }
 
 function updateStatus() {
+    let { current, gameOver, winner } = gameStatus;
     const nextStatusText = document.querySelector("#next") as HTMLParagraphElement;
     if (gameOver) {
         nextStatusText.innerText = `Winner: ${winner}`;
@@ -140,31 +169,102 @@ function createResetButton() {
     const button = document.createElement("button");
     button.id = "reset";
     button.innerText = "Reset";
-    gameStatus?.appendChild(button);
+    statusText?.appendChild(button);
     button.addEventListener("click", (event) => {
         while (board?.firstChild) {
             board.removeChild(board.firstChild);
         }
-        while (gameStatus?.firstChild) {
-            gameStatus.removeChild(gameStatus.firstChild);
+        while (statusText?.firstChild) {
+            statusText.removeChild(statusText.firstChild);
         }
         init();
     })
+}
+
+function createTimeTravel() {
+    const lists = document.createElement("ul");
+    lists.id = "timeTravel";
+    const list = document.createElement("li");
+    list.innerText = `Go to step 0`;
+    list.addEventListener("click", (event) => onStepClicked(event, 0));
+    lists.appendChild(list);
+    statusText?.appendChild(lists);
+}
+
+function addToTimeTravel() {
+    let lists = document.querySelector("#timeTravel");
+    const childrenLen = lists!.children.length || 0;
+    const list = document.createElement("li");
+    list.innerText = `Go to step ${childrenLen}`;
+    list.addEventListener("click", (event) => onStepClicked(event, childrenLen));
+    lists!.appendChild(list);
+}
+
+function onStepClicked(event: MouseEvent, step: number) {
+    console.log(`step: ${step}`)
+    const gameStatusToShow = deepCopyGameStatus(steps[step]);
+    gameStatus = gameStatusToShow;
+
+    updateStatus();
+    updateBoard();
+}
+
+// update UI board based on gameStatus.board
+function updateBoard() {
+    const boardElement = document.querySelector(".board");
+    const rowsElement = boardElement?.querySelectorAll(".row");
+    for(let row=0; row<ROW_COUNT; row++) {
+        const rowElement = rowsElement![row];
+        const buttonsElement = rowElement.querySelectorAll("button");
+        for(let col=0; col<COL_COUNT; col++) {
+            const buttonElement = buttonsElement[col];
+            buttonElement.innerText = gameStatus.gameBoard[row][col];
+        }
+    }
+}
+
+function deepCopyGameStatus(gameStatusToCopy: GameStatus) {
+    let newGameStatus: GameStatus = {
+        gameBoard: [["", "", ""],
+        ["", "", ""],
+        ["", "", ""]],
+        current: gameStatusToCopy.current,
+        gameOver: gameStatusToCopy.gameOver,
+        winner: gameStatusToCopy.winner,
+        currentStep: gameStatusToCopy.currentStep
+    };
+    
+    for(let row=0; row<ROW_COUNT; row++) {
+        for(let col=0; col<COL_COUNT; col++) {
+            newGameStatus.gameBoard[row][col] = gameStatusToCopy.gameBoard[row][col];
+        }
+    }
+
+    return newGameStatus;
 }
 
 function createGame() {
     createButtons();
     createStatus();
     createResetButton();
+    createTimeTravel();
 }
 
 function init() {
     createGame();
-    gameBoard = [["", "", ""],
-                 ["", "", ""],
-                 ["", "", ""]]
-    current = "X";
+
+    gameStatus = {
+        gameBoard: [["", "", ""],
+        ["", "", ""],
+        ["", "", ""]],
+        current: "X",
+        gameOver: false,
+        winner: "",
+        currentStep: 0
+    }
     movesMade = 0;
-    gameOver = false;
+    steps = [];
+
+    steps.push(deepCopyGameStatus(gameStatus));
 }
 init();
